9fd2b5d1a8800b0946e31fe5cf232598fbaae621
Varun Choudhary <varunchoudhary828@gmail.com>
2024-09-09 20:24:31 +0530

    vercel 2

--- api/app.js
+++ api/app.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const imageRoutes = require('./route/imageRoute');
+const imageRoutes = require('../route/imageRoute');
 const { upload } = require('firebase-storage');
-const uploadRoutes = require('./route/updateRoute');
+const uploadRoutes = require('../route/updateRoute');
 
 const app = express();
 const cors = require('cors');
